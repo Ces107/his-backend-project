61c146a748e1d66acf3d21e6bd5892dc6b1f86f3
César Pereiro García <cpereiro@laberit.com>
2024-10-22 03:35:43 +0200

    Implementacion CRUD de usuarios y servicios de otras entidades

---
+++ src/main/java/com/laberit/sina/bootcamp/modulo5/mini_sina_by_cesar/controller/UserManagementController.java
@@ -0,0 +1,4 @@
+package com.laberit.sina.bootcamp.modulo5.mini_sina_by_cesar.controller;
+
+public class UserManagementController {
+}
---
+++ src/main/java/com/laberit/sina/bootcamp/modulo5/mini_sina_by_cesar/service/DiagnosisService.java
@@ -0,0 +1,4 @@
+package com.laberit.sina.bootcamp.modulo5.mini_sina_by_cesar.service;
+
+public interface DiagnosisService {
+}
